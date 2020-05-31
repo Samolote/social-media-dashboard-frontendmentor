b2eb64197f37b1d2bf08c050aa387c81b513003b
Wojciech Łukowski <wojtek304@gmail.com>
2020-05-31 22:37:54 +0200

    Create functionality which remembers chosen theme and applies it on page load

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 const toggle = document.querySelector('.toggle__checkbox');
 const dashboard = document.querySelector('.dashboard');
 
-const toggleHandler = () =>
-	dashboard.classList.contains('dashboard--dark')
-		? dashboard.classList.replace('dashboard--dark', 'dashboard--light')
-		: dashboard.classList.replace('dashboard--light', 'dashboard--dark');
+const toggleHandler = () => {
+	const isDark = dashboard.classList.contains(dark);
+	if (isDark) {
+		dashboard.classList.replace(dark, light);
+		localStorage.setItem('theme', light);
+	} else {
+		dashboard.classList.replace(light, dark);
+		localStorage.setItem('theme', dark);
+	}
+};
 
 toggle.addEventListener('click', toggleHandler);
+
+const theme = localStorage.getItem('theme');
+const dark = 'dashboard--dark';
+const light = 'dashboard--light';
+
+if (theme) {
+	if (theme === dark) {
+		dashboard.classList.replace(light, dark);
+		toggle.checked = true;
+	} else {
+		dashboard.classList.replace(dark, light);
+		toggle.checked = false;
+	}
+}
